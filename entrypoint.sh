00483aeb298d6d0c7e718c850cb597fc17d58f49
Pedro-Arias <pedroarmila@gmail.com>
2024-11-18 10:53:22 -0500

    Correrct name module

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ python manage.py migrate --no-input
 python manage.py collectstatic --no-input
 python manage.py makemigrations --no-input
 python -m spacy download es_core_news_sm --no-input
-gunicorn testOpenAI.wsgi:application --bind 0.0.0.0 --timeout 90
+gunicorn testOpenAi.wsgi:application --bind 0.0.0.0 --timeout 90
