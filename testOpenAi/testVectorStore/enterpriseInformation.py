da64b5308cbc3f62c02f6ac5206d0b21a3fd123a
Pedro-Arias <60678110+Pedro-Arias@users.noreply.github.com>
2025-01-27 16:15:40 -0500

    Add first steps to terms conditions reading

---
+++ testOpenAi/testVectorStore/enterpriseInformation.py
@@ -0,0 +1,16 @@
+from .models import *
+class Company():
+
+    def __init__(self,id_company):
+        self.id_company = id_company
+
+    def get_company(self, query):
+        company = None
+        try:
+            line_defense = Company.objects.get(id=query)
+        except Exception as e:
+            if "get() returned more than one Company" in str(e):
+                return "ERROR BY COMPANY DUPLICATED"
+        return company
+
+    
--- testOpenAi/testVectorStore/models.py
+++ testOpenAi/testVectorStore/models.py
@@ -1,3 +1,7 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    id_company = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id_assistant = models.CharField(max_length=30)
+    id_vector_store = models.CharField(max_length=30)
